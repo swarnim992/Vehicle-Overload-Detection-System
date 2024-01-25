c2d5b394b5d4025791f0cdd99ae26bac7c88f2c5
swarnim992 <90375441+swarnim992@users.noreply.github.com>
2024-01-25 22:10:25 +0530

    Add files via upload

---
+++ load cell2.ino
@@ -0,0 +1,123 @@
+
+#include <SoftwareSerial.h>
+
+#include <HX711_ADC.h>
+#if defined(ESP8266)|| defined(ESP32) || defined(AVR)
+#include <EEPROM.h>
+#endif
+SoftwareSerial mySerial(8, 7);
+  float i ;
+  int threshold_value =500;
+
+//pins:
+const int HX711_dout = A0; //mcu > HX711 dout pin
+const int HX711_sck = A1; //mcu > HX711 sck pin
+
+//HX711 constructor:
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+const int calVal_eepromAdress = 0;
+unsigned long t = 0;
+
+void setup() {
+  Serial.begin(57600); delay(10);
+    mySerial.begin(9600);
+  Serial.println();
+  Serial.println("Starting...");
+
+  LoadCell.begin();
+  //LoadCell.setReverseOutput(); //uncomment to turn a negative output value to positive
+  float calibrationValue; // calibration value (see example file "Calibration.ino")
+  calibrationValue = 696.0; // uncomment this if you want to set the calibration value in the sketch
+#if defined(ESP8266)|| defined(ESP32)
+  //EEPROM.begin(512); // uncomment this if you use ESP8266/ESP32 and want to fetch the calibration value from eeprom
+#endif
+  EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you want to fetch the calibration value from eeprom
+
+  unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
+  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
+  LoadCell.start(stabilizingtime, _tare);
+  if (LoadCell.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1);
+  }
+  else {
+    LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
+    Serial.println("Startup is complete");
+  }
+}
+
+void loop() {
+  static boolean newDataReady = 0;
+  const int serialPrintInterval = 0; //increase value to slow down serial print activity
+
+  // check for new data/start next conversion:
+  if (LoadCell.update()) newDataReady = true;
+
+  // get smoothed value from the dataset:
+  if (newDataReady) {
+    if (millis() > t + serialPrintInterval) {
+      i = LoadCell.getData();
+      Serial.print("Load_cell output val: ");
+      Serial.println(i);
+      newDataReady = 0;
+      t = millis();
+    }
+  }
+
+  // receive command from serial terminal, send 't' to initiate tare operation:
+  if (Serial.available() > 0) {
+    char inByte = Serial.read();
+    if (inByte == 't') LoadCell.tareNoDelay();
+  }
+
+  // check if last tare operation is complete:
+  if (LoadCell.getTareStatus() == true) {
+    Serial.println("Tare complete");
+  }
+
+
+
+  if (i>= threshold_value)
+  {
+Serial.println("Your Vehicle is Overloded");
+Gsm();
+delay(100000);    
+  }
+
+}
+
+
+void Gsm()
+
+{
+  Serial.println("Initializing..."); 
+    delay(1000);
+
+    mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
+    updateSerial();
+
+    mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
+    updateSerial();
+  mySerial.println("AT+CMGS=\"+919284254871\"");//change ZZ with country code and xxxxxxxxxxx with phone number to sms
+    updateSerial();
+    mySerial.print("Your Vehicle is Overloded and the weight is "); //text content
+    updateSerial();
+       mySerial.println(i);// The SMS text you want to send
+    updateSerial();
+    mySerial.write(26);
+}
+
+
+void updateSerial()
+{
+  delay(500);
+  while (Serial.available()) 
+  {
+    mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while(mySerial.available()) 
+  {
+    Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
+  }
+}
